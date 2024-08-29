69bfe2a1911e93bc5f80e7b351849b5dcc6535db
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-08-29 19:37:33 +0530

    Update AboutMe.tsx

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -12,9 +12,9 @@ const AboutMe = () => {
    
       <div className='text-lg tracking-wide flex flex-col gap-5' data-aos="fade-in">
         <p>
-       I’m a web developer with solid experience in React.js and Next.js, specializing in building and optimizing user-friendly, responsive web applications. My expertise spans front-end development and backend-as-a-service integration, with a strong track record in crafting and maintaining single-page applications, seamlessly integrating REST APIs, and ensuring responsive, mobile-first designs.
+       Im a web developer with solid experience in React.js and Next.js, specializing in building and optimizing user-friendly, responsive web applications. My expertise spans front-end development and backend-as-a-service integration, with a strong track record in crafting and maintaining single-page applications, seamlessly integrating REST APIs, and ensuring responsive, mobile-first designs.
 
-I’m proficient in leveraging tools like Webpack, npm, and Git for efficient development and deployment processes. Beyond my core technical skills, I’m also expanding my knowledge in Machine Learning, enhancing my ability to tackle complex challenges.
+I am proficient in leveraging tools like Webpack, npm, and Git for efficient development and deployment processes. Beyond my core technical skills, I am also expanding my knowledge in Machine Learning, enhancing my ability to tackle complex challenges.
 
 I thrive in collaborative environments, working effectively with cross-functional teams, and I’m always ready to take on new challenges, adapt to emerging technologies, and deliver high-quality solutions.
         {/* <p data-aos="fade-right">
