6a8801596dfe1a0e234ea3cb3a57bd520944a610
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-09-04 18:43:47 +0530

    Update ProjectsContainer.tsx

--- components/ProjectsContainer.tsx
+++ components/ProjectsContainer.tsx
@@ -15,7 +15,7 @@ const ProjectsContainer: React.FC<ProjectsContainerProps> = ({ img, text, link }
   <div className='w-80 h-96 border-[1px] border-blue-600 overflow-hidden relative rounded-lg group '  >
    <Image className='object-cover translate-y-0 group-hover:-translate-y-[65%] transition-transform duration-[5s]'
      width={350} height={350} src={img} 
-    alt="AmazonClone"/>
+    alt="project"/>
    <p className='absolute hidden group-hover:inline-block bottom-0 w-full py-1 bg-blue-600 text-white text-center text-sm font-semibold hover:duration-[3s] pl-1 pr-1'>
      {text}</p>
    </div>
