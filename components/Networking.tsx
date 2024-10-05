71783137a5f487f6144d911b362c4ca48335329c
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-10-06 04:02:08 +0530

    Update Networking.tsx

--- components/Networking.tsx
+++ components/Networking.tsx
@@ -1,31 +1,31 @@
 
-// import SkillsContainer from './SkillsContainer'
-// import {SiJavascript} from 'react-icons/si'
-// import { GiSkills } from "react-icons/gi";
-import Title from "./Title";
-import NetworkingContainer from './NetworkingContainer'
+// // import SkillsContainer from './SkillsContainer'
+// // import {SiJavascript} from 'react-icons/si'
+// // import { GiSkills } from "react-icons/gi";
+// import Title from "./Title";
+// import NetworkingContainer from './NetworkingContainer'
 
 
-const Networking = () => {
+// const Networking = () => {
  
-    return (
-      <div className="wrapper" data-aos='fade-right'>
-        <Title text="Networking" icon='' />
-        <div className="flex gap-4 flex-wrap">
-          <NetworkingContainer
-            title="HTTP" link=""/>
-{/*           <NetworkingContainer 
-          title="HTTPS" link="" /> */}
-          <NetworkingContainer 
-          title="FTP" link="" />
-          <NetworkingContainer 
-          title="TCP/IP" link="" />
-          <NetworkingContainer
-          title="DNS"
-          link=""
-          />
-        </div>
-      </div>
-    );
-  };
-export default Networking
+//     return (
+//       <div className="wrapper" data-aos='fade-right'>
+//         <Title text="Networking" icon='' />
+//         <div className="flex gap-4 flex-wrap">
+//           <NetworkingContainer
+//             title="HTTP" link=""/>
+// {/*           <NetworkingContainer 
+//           title="HTTPS" link="" /> */}
+//           <NetworkingContainer 
+//           title="FTP" link="" />
+//           <NetworkingContainer 
+//           title="TCP/IP" link="" />
+//           <NetworkingContainer
+//           title="DNS"
+//           link=""
+//           />
+//         </div>
+//       </div>
+//     );
+//   };
+// export default Networking
