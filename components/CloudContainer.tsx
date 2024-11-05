af6842f02fbd8296b986fcd23b31c0305e9370dc
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-11-05 14:43:27 +0530

    Update CloudContainer.tsx

--- components/CloudContainer.tsx
+++ components/CloudContainer.tsx
@@ -7,7 +7,7 @@ interface CloudContainerProps {
 
 const CloudContainer: React.FC<CloudContainerProps> = ({ title, icon }) => {
   return (
-    <div className="flex items-center gap-2"> {/* Aligns children with flex */}
+    <div className="flex items-center gap-2"> 
       <span className="w-12 h-12 rounded-full flex items-center justify-center text-5xl">
         {icon}
       </span>
