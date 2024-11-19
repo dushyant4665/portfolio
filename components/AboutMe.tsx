c86d3378ec6b60fcaca1bcca1a2f624624e17d1c
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-11-19 15:49:02 +0530

    Update AboutMe.tsx

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -5,18 +5,18 @@ import { SiInformatica } from "react-icons/si";
 
 
 
-const AboutMe = () => {
+const AboutMe=()=>{
   return (
     <div className='wrapper' data-aos="fade-in" >
      <Title text='About Me' icon={<SiInformatica className='text-white'/>}/>
    
       <div className='text-lg tracking-wide flex flex-col gap-5' data-aos="fade-in">
         <p>
-        I am a web developer with solid experience in React.js and Next.js, specializing in building and optimizing user-friendly, responsive web applications. My expertise spans front-end development and backend-as-a-service integration, with a strong track record in crafting and maintaining single-page applications, seamlessly integrating REST APIs, and ensuring responsive, mobile-first designs.
+      I am a web developer who loves creating easy-to-use and mobile-friendly websites. I work with React.js and Next.js to build cool web apps. I also connect these apps to REST APIs and make sure they look good on all devices.
 
-I am proficient in leveraging tools like Webpack, npm, and Git for efficient development and deployment processes. Beyond my core technical skills, Im also expanding my knowledge in Machine Learning, enhancing my ability to tackle complex challenges.
+I use tools like Webpack, npm, and Git to make my work faster and better. Lately, I ve been learning about Machine Learning to try new things and solve harder problems.
 
-I thrive in collaborative environments, working effectively with cross-functional teams, and Im always ready to take on new challenges, adapt to emerging technologies, and deliver high-quality solutions.
+I like working with teams, learning new stuff, and taking on fun challenges. My aim is to make websites that people enjoy using!
         </p>
       
       </div>
