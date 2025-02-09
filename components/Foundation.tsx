0be130e2082c5c2cc510c145f86850a63c85e7b4
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2025-02-09 14:18:34 +0530

    Update Foundation.tsx

--- components/Foundation.tsx
+++ components/Foundation.tsx
@@ -1,41 +1,4 @@
 
-// // import SkillsContainer from './SkillsContainer'
-// // import {SiJavascript} from 'react-icons/si'
-// import { GiSkills } from "react-icons/gi";
-// import Title from "./Title";
-// import FoundationContainer from './FoundationContainer'
-
-
-// const Foundation = () => {
- 
-//     return (
-//       <div className="wrapper" data-aos='fade-right'>
-//         <Title text="Computer Science Foundations" icon='' />
-//         <div className="flex gap-4 flex-wrap">
-//           <FoundationContainer
-//             title="Data Structures and Algorithm" link=""/>
-//             <p> and </p>
-// {/*           <NetworkingContainer 
-//           title="HTTPS" link="" /> */}
-//           <FoundationContainer 
-//           title="Object Oriented Programing" link="" />
-//           {/* <FoundationContainer 
-//           title="TCP/IP" link="" />
-//           <FoundationContainer
-//           title="DNS"
-//           link=""
-//           /> */}
-//         </div>
-//       </div>
-//     );
-//   };
-// export default Foundation
-
-
-
-
-
-
 import { GiSkills } from "react-icons/gi";
 import Title from "./Title";
 import FoundationContainer from './FoundationContainer';
